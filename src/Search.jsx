a9f825b7543b3793c8f3d55fb0a8771d2e284787
RohanJohnson <21000165@students.gateshead.ac.uk>
2023-01-31 11:05:26 +0000

    up to date files, popular search works using baseUrl of movtest. new search not working

--- src/Search.jsx
+++ src/Search.jsx
@@ -24,35 +24,82 @@ export default function Search() {
   );
 }
 
-
-const baseURL = 'http://localhost:3000/movtest';
+// const baseURL = 'http://localhost:3000/movtest';
+const baseURL = 'http://localhost:3000/search';
 
 
 export async function GetMovie(event) {
   event.preventDefault();
-try{
-  const searchURL = baseURL+"?title="+document.getElementById('search').value;
-const config = {
-  method: 'get',
-  url: searchURL,
-  headers: {}
+  try {
+    const searchURL = baseURL + "?title=" + document.getElementById('search').value;
+    const config = {
+      method: 'get',
+      url: searchURL,
+      headers: {}
+    };
+    const response = await axios(config);
+    let info = response.data;
+    if (info.length === 0){
+      // newDiv = document.createElement('div');
+      // newError = document.createTextNode('Sorry, movie is not found');
+      // newDiv.appendChild(newError);
+      // return(e);
+      throw new Error('Sorry, movie not found.');
+    }
+    const div = document.getElementById("con");
+    const icon = info[0].poster_path;
+    // const newContent = document.createTextNode(JSON.stringify(info, null, 10));
+    const newIcon = document.createElement("img");
+    const source = ("https://www.themoviedb.org/t/p/w300_and_h450_bestv2" + icon)
+    newIcon.src = source;
+    document.getElementById('con').appendChild(newIcon);
+
+
+    const table = document.createElement("table");
+    
+    info.forEach(obj => {
+      
+      let objKey = (Object.keys(obj));
+      let objValue = (Object.values(obj));
+      let i = 0;
+      objKey.forEach(element => {
+        let newRow = table.insertRow(-1);
+        let newCell = newRow.insertCell(0);
+        let newText = document.createTextNode(element);
+        newCell.appendChild(newText);
+
+        let newCell2 = newRow.insertCell(1);
+        let newText2 = document.createTextNode(objValue[i]);
+        console.log(i);
+        newCell2.appendChild(newText2);
+        i=i+1;
+      })
+  
+    });
+    document.getElementById('con').appendChild(table);
+    
+
+
+    // div.appendChild(newContent);
+    } catch (e) {
+    console.log(e);
+  }
+}
+
+
+// window.onbeforeunload = function () {
+//   if(confirm("Are you sure you want to refresh?")){
+//     window.scrollTo(0, 0);
+//   }
+// }
+
+window.onbeforeunload = (event) => {
+  const e = event || window.event;
+  window.scrollTo(0, 0);
+  // Cancel the event
+  e.preventDefault();
+  if (e) {
+    e.returnValue = ''; // Legacy method for cross browser support
+  }
+  return ''; // Legacy method for cross browser support
 };
-  const response = await axios(config);
-  const div = document.getElementById("con");
-  const icon = response.data[0].poster_path;
-  const newContent = document.createTextNode(JSON.stringify(response.data,null,10));
-  const newIcon = document.createElement("img");
-  const source = ("https://www.themoviedb.org/t/p/w600_and_h900_bestv2"+icon)
-  newIcon.src=source;
-  document.getElementById('con').appendChild(newIcon);
-  div.appendChild(newContent);
-  // document.getElementById('textbox').innerHTML = "Results: <br> <br {response.data}";
-}
-catch(e){
-  console.log(e);
-}
-}
-
-
-
-
